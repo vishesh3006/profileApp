70eaaf4621252e73b0553b7f9717142bf3838ede
Vishesh Malhotra <42926709+vishesh3006@users.noreply.github.com>
2022-04-06 22:57:04 +0530

    Update ProfileList.js

--- src/components/ProfileList.js
+++ src/components/ProfileList.js
@@ -5,7 +5,7 @@ export const ProfileList = ({users, curUser}) => {
     console.log(users)
 
     return (
-        <div style={{marginTop: '50px'}}>
+        <div style={{marginTop: '60px'}}>
             {
                users && users.length>0 && users.map(user => <ProfileCard key={user.uid} curUser={curUser} user={user}/>)
             }
